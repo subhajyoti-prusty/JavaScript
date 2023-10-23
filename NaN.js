f49d84a4fde5c27d1db8b1e5bc159f47ea0ecfa8
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-10-23 13:21:35 +0530

    Add files via upload

---
+++ NaN.js
@@ -0,0 +1,13 @@
+// NaN :- It is Not-a-Number
+ let a = 0;
+ let nan = a/a;
+ let nan1 = NaN - 1;
+ let nan2 = NaN + 1;
+ let nan3 = NaN * 1;
+ let nan4 = NaN / 1;
+ console.log(nan);
+ console.log(nan1);
+ console.log(nan2);
+ console.log(nan3);
+ console.log(nan4);
+ console.log("Hello world");
