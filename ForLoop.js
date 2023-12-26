7538dcf48183940b617d6ad4b8b7fade1851ced9
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-12-26 18:38:15 +0530

    Add files via upload

---
+++ ForLoop.js
@@ -0,0 +1,8 @@
+
+for(let i = 1; i<=10; i++){
+    console.log(i);
+}
+
+for(let i = 10; i>=1; i--){
+    console.log(i);
+}
---
+++ MultipilcationTable.js
@@ -0,0 +1,6 @@
+let n = prompt("Write your number");
+n = parseInt(n);
+
+for(let i=n; i<=n*10; i=i+n){
+    console.log(i);
+}
